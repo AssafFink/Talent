71ad73c7d1e891e38746b5a96d4d68f3455216e2
Lior-Trajtman <trajtmanlior@gmail.com>
2025-01-30 14:27:45 +0200

    Add colors

--- StudentNames.cs
+++ StudentNames.cs
@@ -8,6 +8,9 @@ internal static class StudentNames
     }
     public static void Lior()
     {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.Red;
         Console.WriteLine("Lior");
+        Console.ResetColor();
     }
 }

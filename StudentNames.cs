6aec55c3e77037cd29b8e8bff1fa4bd3b9030699
TalStre <stal999idt@gmail.com>
2025-01-30 14:27:39 +0200

    Add colors

--- StudentNames.cs
+++ StudentNames.cs
@@ -10,6 +10,7 @@ internal static class StudentNames
     public static void Tal()
     {
         Console.ForegroundColor = ConsoleColor.Green;
+        Console.BackgroundColor = ConsoleColor.Red;
         Console.WriteLine("Tal Streshinski");
         Console.ResetColor();
     }

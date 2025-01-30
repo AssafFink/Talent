1e67fd014853a194a606f4b4c129fd8a405b5310
danielaminov <danielaminov114@gmail.com>
2025-01-30 14:25:47 +0200

    add colors

--- StudentNames.cs
+++ StudentNames.cs
@@ -8,6 +8,9 @@ internal static class StudentNames
     }
     public static void Aminov()
     {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.WriteLine("Teacher: Aminov");
+        Console.ResetColor();
     }
 }

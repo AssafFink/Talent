a714135076895edb6406d08d68f06c241e76a0b0
Slaharog <yt1412@gmail.com>
2025-01-30 14:24:54 +0200

    Add Yonatan

--- StudentNames.cs
+++ StudentNames.cs
@@ -6,4 +6,9 @@ internal static class StudentNames
     {
         Console.WriteLine("Teacher: Assaf");
     }
+
+    public static void Yonatan()
+    {
+        Console.WriteLine("Yonatan");
+    }
 }

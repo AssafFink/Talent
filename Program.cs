5cdf9ac815f7c4978be1e01a5923abe493aca56b
Orel009 <orelll009@gmail.com>
2025-01-30 14:28:17 +0200

    Orelnew

--- Program.cs
+++ Program.cs
@@ -10,5 +10,7 @@ internal class Program
 
         StudentNames.Orel();
 
+
+
     }
 }
--- StudentNames.cs
+++ StudentNames.cs
@@ -10,5 +10,31 @@ internal static class StudentNames
     public static void Orel()
     {
         Console.WriteLine("Orel");
+        Console.WriteLine("-----------------");
+        string name = "orel";
+        int length = name.Length;
+        int width = 2 * length - 1;
+
+        for (int row = 0; row < length; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                int center = length - 1;
+                int left = center - row;
+                int right = center + row;
+
+           
+                if (col == left || col == right || row == length - 1)
+                {
+                    Console.Write(name[col % length]);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
     }
+}
 }
